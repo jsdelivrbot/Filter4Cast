699fe25c8ee56a70935b592d045bbfd07d67bf6e
Gyri Reiersen <gyri.reiersen@tum.de>
2017-08-31 18:45:43 +0200

    Timeseries added

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,7 @@
 const express                  = require('express');
 const router                   = express.Router();
 const request                   = require('request-promise');
+const UAAClient                 = require('predix-uaa-client');
 const dateformat = require('dateformat');
 var fs = require('fs')
     , split = require('split')
@@ -19,14 +20,15 @@ const TS_ZONE_ID = process.env.zoneId ||'067e39c4-de83-40e2-b966-2f83ea5fc292';
 
 var days_beteewn_fchange = 90;
 
-const LONG_DAYS_PERIOD = 2000;
-const SHORT_DAYS_PERIOD=1000;
+//Number of days
+const LONG_DAYS_PERIOD = 30;
+const SHORT_DAYS_PERIOD=10;
 
 
 //----------------------//
 
-var resetDate_l_asDate_co2 = new Date(1499903882588 + 86400000*20)
-var resetDate_l_asDate_co1 = new Date(1499903882588 + 10* 86400000)
+var resetDate_l_asDate_co2 = 1499903882588 + 86400000*20
+var resetDate_l_asDate_co1 = 1499903882588 + 10* 86400000
         
 function max(a,b)
 {
@@ -39,12 +41,168 @@ function min(a,b)
     else return b;
 }
 
+console.log(Date.now());
+
 router.get('/', function(req, res){
     res.send("inside router");
 });
-router.get('/', function(req, res){
-    res.send("inside router");
+
+// VALUES POSTED FROM TIMESERIES
+router.post('/getTopTagspost',function(req,res){
+
+    const rawBody = {
+        "start": '1y-ago',
+        "tags": [{
+            "name": req.body.tag,
+            "order": "asc"
+        }]
+    };
+
+    UAAClient.getToken(UAA_URL, CLIENT_ID, CLIENT_SECRET).then(token => {
+        request({
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + token.access_token,
+                        'Predix-Zone-Id': TS_ZONE_ID
+},
+    uri: process.env.timeseriesURL || 'https://time-series-store-predix.run.aws-usw02-pr.ice.predix.io/v1/datapoints',
+        body: rawBody,
+        method: 'POST',
+        json: true
+}).then(data => {
+            console.log(data.tags[0].name);
+    var date_voc_long = (Date.now() - LONG_DAYS_PERIOD * 86400000); 
+            
+    var date_voc_short = (Date.now() - SHORT_DAYS_PERIOD * 86400000);
+            
+            var date_reset = "";
+            if(data.tags[0].name == "unternehmer-3dlaser-co1")  date_reset = resetDate_l_asDate_co1;
+            else if (data.tags[0].name == "unternehmer-3dlaser-co2") date_reset = resetDate_l_asDate_co2;
+            else console.log("YOLO - wrong name");
+                
+    var dateNewFormat = dateformat(date_reset, "dd-mm-yyyy HH:MM:ss")        
+    var changeRequired = true;
+    var numDays = 0;
+
+    var total_voc_long = 0;
+    var total_voc_short = 0;
+    var total_voc_current = 0;
+            
+    var sum_voc_long_per_day = 0;
+    var sum_voc_short_per_day = 0;
+    var sum_voc_current_per_day = 0;
+            
+    var date_of_record = 0;
+    var nextDay = 0;
+
+    var limit = 0;
+    limit = data.tags[0].results[0].values.length // all of the dataset from first day of measurement
+    
+    var totalSumCurrentPlot=0;
+    var plotValues = [];
+    var plotTimes = [];
+    var datatemp = [];
+    for (i = 0; i < limit; i++) { 
+
+        if (changeRequired == true) {
+            nextDay = (data.tags[0].results[0].values[i][0] + 86400000) ;
+            changeRequired = false;
+            total_voc_long += sum_voc_long_per_day;
+            total_voc_short += sum_voc_short_per_day;
+            total_voc_current += sum_voc_current_per_day;
+            sum_voc_long_per_day = 0;
+            sum_voc_short_per_day = 0;
+            sum_voc_current_per_day = 0; 
+            numDays++;
+            console.log("numDays " + numDays);
+            console.log("total_voc_long " + total_voc_long);
+
+        }
+        console.log("date_voc_long " + date_voc_long);
+
+
+        currentTime = data.tags[0].results[0].values[i][0];
+        console.log("currentTime " + currentTime);
+        console.log("nextDay " + nextDay);
+        console.log("date_voc_long " + date_voc_long);
+        console.log("date_voc_short " + date_voc_short);
+        console.log("date_reset " + date_reset);
+
+        if (currentTime < nextDay && currentTime> date_voc_long) {
+            sum_voc_long_per_day += parseInt(data.tags[0].results[0].values[i][1]);
+        }
+        if (currentTime < nextDay && currentTime > date_voc_short) {
+            sum_voc_short_per_day += parseInt(data.tags[0].results[0].values[i][1]);
+        }
+
+        if (currentTime < nextDay && currentTime > date_reset) {
+            sum_voc_current_per_day += parseInt(data.tags[0].results[0].values[i][1]);
+            totalSumCurrentPlot+=parseInt(data.tags[0].results[0].values[i][1]);
+            plotValues.push(totalSumCurrentPlot);
+            plotTimes.push(currentTime);
+            var val = [currentTime, totalSumCurrentPlot ];
+            
+            datatemp.push(val);
+        }
+        if (currentTime >= nextDay) {
+            changeRequired = true
+        }
+    }
+    total_voc_long += sum_voc_long_per_day;
+    total_voc_short += sum_voc_short_per_day;
+    total_voc_current += sum_voc_current_per_day;
+    console.log("numDays " + numDays);
+    console.log("total_voc_long " + total_voc_long);
+    console.log("total_voc_short " + total_voc_short);
+    console.log("total_voc_current " + total_voc_current);
+
+    var new_long_days = min(LONG_DAYS_PERIOD, numDays);
+    var new_short_days = min(SHORT_DAYS_PERIOD, numDays);
+
+    var average_voc_long = total_voc_long / new_long_days;
+    var average_voc_short = total_voc_short / new_short_days;
+    console.log("avg voc long " + average_voc_long)
+    console.log("avg voc short " + average_voc_short)
+    
+    var max_voc = days_beteewn_fchange * average_voc_long;  // calculating the maximal VOC amount until filter has to be changed
+
+    console.log("Max Voc " + max_voc);
+
+    var prediction = (max(0, max_voc - total_voc_current) / (average_voc_short)).toFixed(0);
+    console.log("Prediction - days until change: " + prediction );
+    
+    var filterpercentage = (((days_beteewn_fchange - prediction) / days_beteewn_fchange)*100).toFixed(2)
+    
+    plotValues.push(max_voc);
+    var a= currentTime + prediction * 86400000;
+    var val = [a, max_voc];
+            console.log(data.tags[0].results[0].values[4][0]);
+            
+    datatemp.push(val);
+    
+    var YOLO = {
+        "prediction": prediction,
+        "filterName": data.tags[0].name,
+        "lastChange": dateNewFormat,
+        "filterpercentage": filterpercentage,
+        "plotValues": plotValues,
+        "plotTimes": plotTimes,
+        "all": datatemp
+        
+    }
+    res.send(YOLO );
+
+}).catch(err => {
+        console.log('Error:', err);
 });
+
+}).catch((err) => {
+        console.error('Error getting token', err);
+});});
+
+
+
+
 // VALUES RETRIEVED FROM API
 router.post('/getValues',function(req,res){
 
@@ -119,7 +277,7 @@ router.post('/getValues',function(req,res){
 
     var max_voc = days_beteewn_fchange * average_voc_long;  // calculating the maximal VOC amount until filter has to be changed
 
-    console.log("" + max_voc);
+    console.log("Max Voc " + max_voc);
 
     var prediction = max(0, max_voc - total_voc_current) / average_voc_short;
     console.log("" + prediction);
@@ -134,21 +292,20 @@ router.post('/getValues',function(req,res){
 // VALUES RETRIEVED FROM FILE
 router.post('/getValuesFile',function(req,res) {
 
-    //var resetDate_l = (new Date(req.body.resetDate)).getTime() / 1000;
-
+    var filterName = "";
     var resetDate_l_asDate = "";
     if(req.body.feed_key == "co1"){
         var data = JSON.parse(fs.readFileSync("app/public/co1-20170829-1050.json", 'utf8'));
         resetDate_l_asDate = resetDate_l_asDate_co1;
         var resetDate_l = (new Date(resetDate_l_asDate_co1)).getTime() / 1000
-        var filterName = "Filter Lasercutter 2 (Sensor CO1)"
+         filterName = "Filter Lasercutter 2 (Sensor CO1)"
         }
     
     else{
         var data = JSON.parse(fs.readFileSync("app/public/co2-20170829-1048.json", 'utf8'));
         resetDate_l_asDate = resetDate_l_asDate_co2;
         var resetDate_l = (new Date(resetDate_l_asDate_co2)).getTime() / 1000
-        var filterName = "Filter Lasercutter 1 (Sensor CO2)"
+         filterName = "Filter Lasercutter 1 (Sensor CO2)"
         }
     console.log("resetdate: " + resetDate_l_asDate );
 
@@ -158,7 +315,6 @@ router.post('/getValuesFile',function(req,res) {
     var date_voc_short = (Date.now() - SHORT_DAYS_PERIOD * 86400000) / 1000;
 
     var date_reset = resetDate_l;
-    //date_reset.setDate(resetDate);
     var dateNewFormat = dateformat(resetDate_l_asDate, "dd-mm-yyyy HH:MM:ss")
 
     var changeRequired = true;
@@ -260,7 +416,7 @@ router.post('/getValuesFile',function(req,res) {
     var a= new Date(currentTime + prediction);
     var val = [a*1000, max_voc];
             
-            datatemp.push(val);
+    datatemp.push(val);
     
     var data = {
         "prediction": prediction,
